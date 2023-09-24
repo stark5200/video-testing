99c5e5d10bad6dd864e16e0a32390010d55c3bd6
stark5200 <a.taha5200@gmail.com>
2023-09-24 23:07:12 +0200

    360 testing

---
+++ src/components/360.jsx
@@ -0,0 +1,15 @@
+
+function BGVideo() {
+  return (
+    <div className="main">
+      <div className="overlay">
+        <div className="content">
+          <h1>360 Images</h1>
+          <p>Testing.</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default BGVideo;
