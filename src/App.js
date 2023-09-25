5f2e02d7a7262b1bd5b9689b8995f743958556eb
stark5200 <a.taha5200@gmail.com>
2023-09-25 21:53:43 +0200

    ThreeSixtyView

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
 import BGVideo from "./components/BGVideo"
 import VideoPlayer from './components/VideoPlayer';
+import ThreeSixtyView from './components/ThreeSixtyView';
 
 function App() {
   return (
     <div className="App">
+      {/*
       <BGVideo />
       <VideoPlayer />
+      */}
+      <ThreeSixtyView />
     </div>
   );
 }
---
+++ src/components/ThreeSixtyView.jsx
@@ -0,0 +1,16 @@
+
+function ThreeSixtyView() {
+  return (
+    <div className="main">
+      <div className="overlay">
+        <div className="content">
+          <h1>360 Images</h1>
+          <p>Testing.</p>
+          <iframe width="100%" height="400" src="https://tour-de.metareal.com/apps/player?asset=bb783986-966e-4695-94e9-3d2cef2cec85&starting=tour&autostart=false" frameborder="0" allow="xr-spatial-tracking *; accelerometer *; magnetometer *; gyroscope *" allowvr="yes" allowfullscreen="yes"></iframe>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ThreeSixtyView;
