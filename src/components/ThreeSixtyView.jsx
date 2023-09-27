45dc955ebcabf34a4cce26d63e75104a392ec24b
stark5200 <a.taha5200@gmail.com>
2023-09-27 21:39:40 +0200

    foreground

--- src/components/ThreeSixtyView.jsx
+++ src/components/ThreeSixtyView.jsx
@@ -4,7 +4,7 @@ function ThreeSixtyView() {
     <div className="main">
       <div className="overlay">
         <div className="threesixty">
-          <iframe width="80%" height="800" title="360 View" src="https://tour-de.metareal.com/apps/player?asset=bb783986-966e-4695-94e9-3d2cef2cec85&starting=tour&autostart=false" frameborder="0" allow="xr-spatial-tracking *; accelerometer *; magnetometer *; gyroscope *" allowvr="yes" allowfullscreen="yes"></iframe>
+          <iframe width="100%" height="800" title="360 View" src="https://tour-de.metareal.com/apps/player?asset=bb783986-966e-4695-94e9-3d2cef2cec85&starting=tour&autostart=false" frameborder="0" allow="xr-spatial-tracking *; accelerometer *; magnetometer *; gyroscope *" allowvr="yes" allowfullscreen="yes"></iframe>
         </div>
       </div>
     </div>
